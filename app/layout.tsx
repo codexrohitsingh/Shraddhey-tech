239d4b1b79a72a1b40d694af48a490008bb403c8
rohit singh <codex.rohit.singh@gmail.com>
2026-06-24 20:53:01 +0530

    signature

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,13 +65,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
       <body className="min-h-full flex flex-col">
         <SmoothScrollProvider>
           <Navbar />
-          <main className="flex-grow">
-            {children}
-          </main>
+          <div style={{ display: "none" }}>
+            Rohit singh email-:rs21rohit@gmail.com
+          </div>
+          <main className="flex-grow">{children}</main>
           <Footer />
         </SmoothScrollProvider>
       </body>
